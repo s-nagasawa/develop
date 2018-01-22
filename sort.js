6860c4a84730122d410049a46b3a084ff77c9170
長澤 賢 <nagasawa@pensees.co.jp>
2018-01-22 18:53:44 +0900

    配列をソートする処理を追加

--- sort.js
+++ sort.js
@@ -1,2 +1,12 @@
-const number = [19, 3, 81, 1,24, 21];
-console.log(number);
+const sortNumber = number => {
+  number.sort((a,b) => {
+    if (a === b) {
+      return 0;
+    }
+    return a < b ? -1 : 1;
+  });
+};
+
+const number = [19, 3, 81, 1, 24, 21];
+sortNumber(number);
+console.log(number);const number = [];
